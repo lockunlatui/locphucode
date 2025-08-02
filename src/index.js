b0c0c8af73d6fea2eba01dff614788737a3eeb7c
Do Xuan Loc <lockunlatui@gmail.com>
2025-08-02 22:18:58 +0700

    deploy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 export default {
   async fetch(request, env, ctx) {
-    return new Response("Hello World!");
+    // This will be handled by Workers Sites automatically
+    // The static files from 'out' folder will be served
+    return new Response("Loading...", {
+      headers: { "Content-Type": "text/html" },
+    });
   },
 };
